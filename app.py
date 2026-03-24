eae53e21e554928e4b656c29a7c3676641d6eb1f
hi_frombg:)ngl <dimitarp758@gmail.com>
2026-03-24 14:01:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,27 @@ if st.button("Добави"):
     st.success(f"{name} е добавено!")
 else:
   st.warning("Попълнете всички полета!")
+# Премахване
+if st.session_state.animals:
+  st.header("Премахни животно")
+  names = []
+  for a in st.session_state.animals:
+    names.append(a["име"])
+  remove_name = st.selectbox("Избери животно за премахване", паmes)
+  if st.button("Премахни"):
+    for a in st.session_state.animals:
+      if a["име"] == remove_name:
+        st.session_state.animals.remove(a)
+        break
+    st.success(f"{remove_name} е премахнато!")
+# Визуализация
+st.header("Галерия")
+if st.session_state.animals:
+  cols st.columns (3)
+  for idx, animal in enumerate (st.session_state.animals):
+    with cols [idx % 3]:
+      st.subheader (animal ["име"])
+      st.image(animal ["картинка"], use_column_width=True)
+      st.write(animal ["описание"])
+else:
+  st.info("Галерията е празна. Добавете животни!")
